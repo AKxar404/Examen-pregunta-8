5b405e152a3b0aed1de9ef7c0c3a44e84ea4119a
KEVIN ALEXANDER ALBAN RUIZ <kalban4@indoamerica.edu.ec>
2024-05-21 14:37:10 -0500

    Agregar archivos de proyecto.

---
+++ Examen pregunta 8/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace worlddd
+{
+    class Program
+    {
+        interface Pais
+        {
+            //métodos abstractos
+            void colorbandera();
+            void size();
+            void idioma();
+
+
+        }
+        //implementacion de clases
+        class Ecuador : Pais
+        {
+            public void colorbandera()
+            {
+                Console.WriteLine("TRICOLOR,amarillo,azul,rojo");
+            }
+            public void size()
+            {
+                // The body of animalSound() is provided here
+                Console.WriteLine("283.561 km²");
+            }
+            public void idioma()
+            {
+                Console.WriteLine("Españolo");
+            }
+
+        }
+        class Brasil : Pais
+        {
+            public void colorbandera()
+            {
+                Console.WriteLine("amarillo,verde, azul");
+            }
+            public void size()
+            {
+                // The body of animalSound() is provided here
+                Console.WriteLine("8,51 millones km²");
+            }
+            public void idioma()
+            {
+                Console.WriteLine("Portuguez");
+            }
+
+        }
+        class Andorra : Pais
+        {
+            public void colorbandera()
+            {
+                Console.WriteLine("azul,amarillo,rojo");
+            }
+            public void size()
+            {
+                // The body of animalSound() is provided here
+                Console.WriteLine("468 km²");
+            }
+            public void idioma()
+            {
+                Console.WriteLine("Catalán");
+            }
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            //aplicacion de crear cualquier animal en base de las clases heredadas con sus metodos de "ANIMAL" y propios
+            Console.WriteLine("Color de la bandera de Ecuador:");
+            Ecuador Ecu = new Ecuador();
+            Ecu.colorbandera();
+            Console.WriteLine("Tamaño de Andorra:");
+            Andorra andoria = new Andorra();
+            andoria.size();
+            Console.WriteLine("Idioma de Brasil:");
+            Brasil Brasilia = new Brasil();
+            Brasilia.idioma();
+
+
+        }
+
+
+    }
+}
